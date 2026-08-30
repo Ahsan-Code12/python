f761067c1dc141e17a7bfc64eeb3f258574e7dfa
ahsan_Code <none31ae@gmail.com>
2026-08-30 23:37:49 +0500

    Update game.py

--- Python/rock paper scissor game/game.py
+++ Python/rock paper scissor game/game.py
@@ -6,12 +6,17 @@ print("computer choice: ", computer_choice)
 print("user choice: ", user_choice)
 
 if computer_choice == user_choice:
-    print("tie")
+    tie = {"user": user_choice, "computer": computer_choice, "tie": True}
+    print(tie)
 elif computer_choice == "rock" and user_choice == "paper":
-    print("user wins")
+    userwin = {"computer choice": computer_choice, "user choice": user_choice, "winner": "user"}
+    print(userwin)
 elif computer_choice == "paper" and user_choice == "scissors":
-    print("user wins")
+    scissorwin = {"computer choice": computer_choice, "user choice": user_choice, "winner": "user"}
+    print(scissorwin)
 elif computer_choice == "scissors" and user_choice == "rock":
-    print("user wins")
+    rockwin = {"computer choice": computer_choice, "user choice": user_choice, "winner": "user"}
+    print(rockwin)
 else:
-    print("computer wins")
+    compwin = {"computer choice": computer_choice, "user choice": user_choice, "winner": "computer"}
+    print(compwin)
