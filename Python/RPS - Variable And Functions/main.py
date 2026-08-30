fe6443086c8431e3e4dbf328b742045f4077015e
ahsan_Code <none31ae@gmail.com>
2026-08-30 18:59:51 +0500

    Update main.py

--- Python/RPS - Variable And Functions/main.py
+++ Python/RPS - Variable And Functions/main.py
@@ -1,7 +1,30 @@
 def get_choices():
-    player_choice = 'rock'
-    computer_choice = 'paper'
-    return player_choice, computer_choice
+    player_choice = input("enter your choice: ")
+    if player_choice == 'rock':
+        computer_choice = 'paper'
+    elif player_choice == 'paper':
+        computer_choice = 'scissors'
+    elif player_choice == 'scissors':
+        computer_choice = 'rock'
+
+    decisions = {"player": player_choice, "comp choice": computer_choice}
+
+    return decisions
 
 choices = get_choices()
 print(choices)
+
+def user_name():
+    username = input("enter your name: ")
+    return username
+
+choice_user_name = user_name()
+
+def user_color():
+    usercolor = input("enter color: ")
+    return usercolor
+
+color = user_color()
+
+dict = {"name": choice_user_name, "color": color}
+print(dict)
