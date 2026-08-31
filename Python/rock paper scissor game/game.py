ebf2c46a7bc79ac8280d8bf96219dec9240c9e92
ahsan_Code <none31ae@gmail.com>
2026-08-31 14:35:18 +0500

    Update game.py

--- Python/rock paper scissor game/game.py
+++ Python/rock paper scissor game/game.py
@@ -3,18 +3,23 @@ import random
 user_choice = input("enter your choice: ")
 computer_choice = random.choice(["rock", "paper", "scissors"])
 
-if computer_choice == user_choice:
-    tie = {"user": user_choice, "computer": computer_choice, "tie": True}
-    print(tie)
-elif computer_choice == "rock" and user_choice == "paper":
-    userwin = {"computer choice": computer_choice, "user choice": user_choice, "winner": "user"}
-    print(userwin)
-elif computer_choice == "paper" and user_choice == "scissors":
-    scissorwin = {"computer choice": computer_choice, "user choice": user_choice, "winner": "user"}
-    print(scissorwin)
-elif computer_choice == "scissors" and user_choice == "rock":
-    rockwin = {"computer choice": computer_choice, "user choice": user_choice, "winner": "user"}
-    print(rockwin)
+if user_choice == computer_choice:
+  tie_match = {"your choice: ": user_choice, "computer choice: ": computer_choice, "winner": "tie"}
+  print(tie_match)
+
+elif user_choice == "rock" and computer_choice == "scissors":
+  rockwin = {"your choice: ": user_choice, "computer choice: ": computer_choice, "winner": "you"}
+  print(rockwin)
+
+elif user_choice == "scissors" and computer_choice == "paper":
+  sciss_win = {"your choice: ": user_choice, "computer chocice: ": computer_choice, "winner": "you"}
+  print(sciss_win)
+
+elif user_choice == "paper" and computer_choice == "rock":
+  paper_win = {"your choice: ": user_choice, "computer choice: ": computer_choice, "winner: ": "you"}
+  print(paper_win)
+
 else:
-    computer_win = {"computer choice": computer_choice, "user choice": user_choice, "winner": "computer"}
-    print(computer_win)
+    computer_wins = {"your choice: ": user_choice, "computer choice: ": computer_choice, "winner: ": "computer"}
+    print(computer_wins)
+    
